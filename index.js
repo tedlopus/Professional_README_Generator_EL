bffa198b565df66b34f336ef713dccd7cb8bba1f
tedlopus <tedlopus@gmail.com>
2022-03-02 23:57:56 -0700

    prompts for questions and package.json added

--- index.js
+++ index.js
@@ -1,26 +1,88 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer
-  .prompt([
+const prompts = () => {
+return inquirer.prompt([
     {
-      type: 'input',
-      message: 'What is your name?',
-      name: 'name',
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "github",
     },
     {
-      type: 'input',
-      message: 'Where are you?',
-      name: 'location',
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
     },
     {
-      type: 'input',
-      message: 'What is the url for your LinkedIn profile?',
-      name: 'LinkedIN',
+      type: "input",
+      message: "Please enter the title of your project.",
+      name: "projectname",
     },
     {
-      type: 'input',
-      message: 'What is the url for your Github page?',
-      name: 'GitHub',
+      type: "input",
+      message:
+        "Please provide a description of what your application does and how it works.",
+      name: "description",
     },
-  ])
+    {
+      type: "input",
+      message: "Please provide a table of contents.",
+      name: "contents",
+    },
+    {
+      type: "list",
+      message:
+        "Please select a license if you used one, otherwise select none.",
+      name: "license",
+      choices: [
+        "None",
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+        "MIT License",
+        'BSD 2-Clause "Simplified" License',
+        'BSD 3-Clause "New" or "Revised" License',
+        "Boost Software License 1.0",
+        "Creative Commons Zero v1.0 Universal",
+        "Eclipse Public License 2.0",
+        "GNU Affero General Public License v3.0",
+        "GNU General Public License v2.0",
+        "GNU Lesser General Public License v2.1",
+        "Mozilla Public License 2.0",
+        "The Unlicense",
+      ],
+    },
+    {
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "What command should be run to run tests?",
+      name: "test",
+    },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "steps",
+    },
+    {
+      type: "input",
+      message: "Please provide instructions on how to use your application",
+      name: "instructions",
+    },
+    {
+      type: "input",
+      message:
+        "If you have any collaborators on your project or if you used any third party assets, please enter that here.",
+      name: "credits",
+    },
+  ]);
+};
+
+  // Function that creates the README
+  function generateReadMe() {
+      fs.writeFile("README.md", prompts, (err) =>
+      err ? console.log(err) : console.log("Generating README...")
+    );
+  };
